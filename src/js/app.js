122bae348145b0eca97eb22dd62d7ff38951de72
Dmitry_home <79811924115@yandex.ru>
2022-11-17 20:47:00 +0300

    random cell toggle

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,25 @@
-import sum from './sum';
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable no-plusplus */
+/* eslint-disable no-console */
+/* global document */
+const button = document.querySelectorAll('button.button');
+const item = document.querySelectorAll('div.item');
 
-console.log('it works!!');
+const arr = [];
+for (let j = 1; j <= 250; j++) {
+  const g = Math.floor(Math.random() * 16);
+  arr.push(g);
+}
+const unicArr = [...new Set(arr)];
+console.log(arr);
+console.log(unicArr);
 
-console.log(sum(2, 2));
+let i = 1;
+button[0].onclick = () => {
+  if (i <= unicArr.length) {
+    item[unicArr[i]].classList.add('test');
+    console.log(i, unicArr[i]);
+    item[unicArr[i - 1]].classList.remove('test');
+    i++;
+  } else console.log('The end');
+};
