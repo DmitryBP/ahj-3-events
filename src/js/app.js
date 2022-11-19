20d0984911353485d1cb3b44a64fe10221934645
Dmitry_home <79811924115@yandex.ru>
2022-11-19 23:33:44 +0300

    cursor added

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable eqeqeq */
 /* eslint-disable implicit-arrow-linebreak */
 /* eslint-disable no-plusplus */
 /* eslint-disable no-console */
@@ -6,20 +7,34 @@ const button = document.querySelectorAll('button.button');
 const item = document.querySelectorAll('div.item');
 
 const arr = [];
-for (let j = 1; j <= 250; j++) {
+for (let j = 0; j <= 150; j++) {
   const g = Math.floor(Math.random() * 16);
   arr.push(g);
 }
 const unicArr = [...new Set(arr)];
-console.log(arr);
-console.log(unicArr);
 
-let i = 1;
+let i = 0;
 button[0].onclick = () => {
-  if (i <= unicArr.length) {
-    item[unicArr[i]].classList.add('test');
-    console.log(i, unicArr[i]);
-    item[unicArr[i - 1]].classList.remove('test');
-    i++;
-  } else console.log('The end');
+  setInterval(() => {
+    if (i < unicArr.length - 1) {
+      if (i == 0) {
+        item[i].classList.add('test');
+      } else {
+        item[0].classList.remove('test');
+        item[unicArr[i]].classList.add('test');
+        if (i != 0) {
+          item[unicArr[i - 1]].classList.remove('test');
+        } else {
+          item[unicArr[i]].classList.remove('test');
+        }
+      }
+      i++;
+    } else console.log('The end');
+  }, 1000);
 };
+const score = 0;
+console.log(score);
+document.addEventListener('click', (e) => {
+  const { target } = e;
+  console.log(target.className.includes('test'));
+});
