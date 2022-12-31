06fc49f779d9e036044a93e737b01e16348de0c3
Dmitry_home <79811924115@yandex.ru>
2022-12-31 20:26:08 +0300

    work game v/2

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,37 @@
+/* eslint-disable no-undef */
 // eslint-disable-next-line import/no-named-as-default
-import ScoreCounter from '../components/scoreCounter/scoreCounter';
 import RandomMooving from '../components/randomMooving/RandomMoov';
 
-ScoreCounter.score();
 // eslint-disable-next-line no-unused-vars, no-undef, import/prefer-default-export
-export const randomMoveing = new RandomMooving('.item');
+const randomMoveing = new RandomMooving('.item');
+
+let heartIcon = document.getElementsByClassName('heart');
+const items = document.querySelectorAll('.item');
+
+let scoreCounter = 0;
+let heartNumber = 2;
+
+const score = document.getElementsByClassName('score');
+items.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    const { target } = e;
+    if (target.className.includes('target')) {
+      scoreCounter += 1;
+      score[0].innerHTML = `Очки: ${scoreCounter}`;
+    } else {
+      heartIcon[heartNumber].classList.add('heartBreak');
+      if (heartNumber > 0) {
+        heartNumber -= 1;
+      } else {
+        randomMoveing.stop();
+        heartNumber = 2;
+        scoreCounter = 0;
+        score[0].innerHTML = `Очки: ${scoreCounter}`;
+        heartIcon = Array.from(heartIcon);
+        heartIcon.forEach((element) => {
+          element.classList.remove('heartBreak');
+        });
+      }
+    }
+  });
+});
